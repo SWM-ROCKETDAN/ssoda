c56f47b0c7b8c46ab91a5891cc72bc8567d3434d
iamhge <fine632@gmail.com>
2021-07-05 21:32:25 +0900

    initialize service-server

---
+++ service-server/src/test/java/com/rocketdan/serviceserver/ServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketdan.serviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
