6f29322c141e5ab4e8ef468e84b18a112c27f471
yjyoon-dev <yjyoon.dev@gmail.com>
2021-07-09 15:29:09 +0900

    setup MaterialApp()

--- app-ui/lib/main.dart
+++ app-ui/lib/main.dart
@@ -9,10 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'HashChecker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primaryColor: Colors.indigoAccent.shade700,
+          accentColor: Colors.indigoAccent.shade700,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Colors.white),
       home: CreateEventStepScreen(),
     );
   }
