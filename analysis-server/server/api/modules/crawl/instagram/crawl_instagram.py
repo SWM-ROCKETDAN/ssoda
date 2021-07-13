29baa76555066351fa874fe900e191218dc6578a
mooyoung2309 <mooyoung2309@gmail.com>
2021-07-13 23:39:54 +0900

    refactor: django server

---
+++ analysis-server/server/api/modules/crawl/instagram/crawl_instagram.py
@@ -0,0 +1,121 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import config
+import re
+import json
+from server.api.modules.crawl import crawl
+import datetime
+from server.api.modules.crawl import proxy
+
+def get_now_time():
+    now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    return now
+
+
+def cal_time_gap(start, end):
+    try:
+        date_time_start = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
+        date_time_end = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%S')
+        gap = date_time_start - date_time_end
+        return str(gap.days) + '-' + str(gap.seconds)
+    except:
+        return ''
+
+
+class CrawlInstagram(crawl.Crawl):
+    def __init__(self, post_url, info_urls):
+        super().__init__(post_url, info_urls)
+
+    def do_crawl_posts(self):
+        for url in self.post_urls:
+            proxy_url = proxy.get_url(url)
+            response = urlopen(proxy_url)
+            soup = BeautifulSoup(response, "html.parser")
+
+            # find data
+            post_data = soup.find('script', type='application/ld+json').string
+            hashtag_data = [item["content"] for item in soup.find_all('meta', property="instapp:hashtags")]
+
+            # convert dictionary
+            post_data = post_data[post_data.find('{'):]
+            json_acceptable_string = post_data.replace("'", "\"")
+            post_data = json.loads(json_acceptable_string)
+
+            # user id
+            if 'author' in post_data:
+                instagram_id = post_data['author']['alternateName'][1:]
+            else:
+                instagram_id = post_data['alternateName'][1:]
+
+            # likes
+            if 'commentCount' in post_data:
+                likes = int(post_data['commentCount'])
+            else:
+                likes = 0
+
+            # comments
+            if 'interactionStatistic' in post_data and 'userInteractionCount' in post_data['interactionStatistic']:
+                comments = int(post_data['interactionStatistic']['userInteractionCount'])
+            else:
+                comments = 0
+
+            # status
+            if post_data['@type'] == 'Person':
+                status = config.PostStatus.DENY
+            else:
+                status = config.PostStatus.ING
+
+            # uploadDate
+            if 'uploadDate' in post_data:
+                upload = post_data['uploadDate']
+            else:
+                upload = ''
+
+            # update
+            update = get_now_time()
+
+            # maintain
+            maintain = cal_time_gap(update, upload)
+
+            # post
+            post = {
+                'url': url,
+                'update': update,
+                'status': status,
+                'type': config.SnsType.INSTAGRAM,
+                'period': {
+                    'upload': upload,
+                    'deleted': '',
+                    'maintain': maintain,
+                },
+                'desc': {
+                    'id': instagram_id,
+                    'likes': likes,
+                    'comments': comments,
+                    'hashtags': hashtag_data,
+                },
+            }
+
+            self.info_urls.append('https://www.instagram.com/' + instagram_id)
+            self.posts.append(post)
+
+    def do_crawl_infos(self):
+        for url in self.info_urls:
+            proxy_url = get_url(url)
+            response = urlopen(proxy_url)
+            soup = BeautifulSoup(response, "html.parser")
+            info_meta = str(soup.find('meta', property="og:description"))
+
+            # preprocessing
+            for c in "., ":
+                info_meta = info_meta.replace(c, "")
+
+            info_nums = re.findall("\d+", info_meta)
+
+            info = {
+                'followers': int(info_nums[0]),
+                'followings': int(info_nums[1]),
+                'posts': int(info_nums[2]),
+            }
+
+            self.infos.append(info)
--- analysis-server/server/config/asgi.py
+++ analysis-server/server/config/asgi.py
@@ -1,10 +1,10 @@
 """
 ASGI config for config project.
 
-It exposes the ASGI callable as a modules-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
