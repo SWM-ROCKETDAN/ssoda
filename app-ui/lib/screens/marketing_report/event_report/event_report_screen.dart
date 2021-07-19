3d31032c14dc10b82df2a5012672b30853ee7cd0
unknown <yjyoon.dev@gmail.com>
2021-07-20 05:18:42 +0900

    refactor code by dividing into individual components

--- app-ui/lib/screens/marketing_report/event_report/event_report_screen.dart
+++ app-ui/lib/screens/marketing_report/event_report/event_report_screen.dart
@@ -1,8 +1,39 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:hashchecker/models/event_report.dart';
 
-class EventReportScreen extends StatelessWidget {
+import 'components/custom_appbar.dart';
+import 'components/expenditure_report.dart';
+import 'components/exposure_report.dart';
+import 'components/header_background.dart';
+import 'components/participation_report.dart';
+import 'components/report_title.dart';
+
+class EventReportScreen extends StatefulWidget {
   const EventReportScreen({Key? key}) : super(key: key);
+
+  @override
+  _EventReportScreenState createState() => _EventReportScreenState();
+}
+
+class _EventReportScreenState extends State<EventReportScreen> {
+  late EventReport eventReport;
+
+  @override
+  void initState() {
+    super.initState();
+    eventReport = EventReport(
+        eventName: '우리가게 SNS 해시태그 이벤트',
+        joinCount: 384,
+        likeCount: 7423,
+        livePostCount: 257,
+        deadPostCount: 127,
+        commentCount: 2184,
+        exposeCount: 51824,
+        followerSum: 51623,
+        followerAvg: 186,
+        costSum: 372100,
+        costPerReward: [50000, 65000, 50000, 75000, 90000]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,195 +42,30 @@ class EventReportScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            height: size.height * 0.35,
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30)),
-            ),
-          ),
+          HeaderBackground(size: size),
           Container(
               padding: const EdgeInsets.only(top: 30),
-              margin: const EdgeInsets.all(20),
+              margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
+                  CustomAppBar(),
                   SizedBox(
-                    height: 40,
+                    height: 30,
                   ),
-                  Text('우리가게 SNS 해시태그 이벤트',
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                  SizedBox(height: 5),
-                  Text('마케팅 성과 보고서',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 28)),
+                  ReportTitle(eventReport: eventReport),
                   SizedBox(
-                    height: 20,
+                    height: 15,
                   ),
                   Expanded(
                     child: SingleChildScrollView(
                       child: Column(
                         children: [
-                          Container(
-                            height: 175,
-                            padding: const EdgeInsets.all(20),
-                            width: size.width,
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  RichText(
-                                      text: TextSpan(
-                                          style: TextStyle(
-                                              color: Colors.black87,
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 18),
-                                          children: [
-                                        TextSpan(text: '총 '),
-                                        TextSpan(
-                                            text: '384',
-                                            style: TextStyle(
-                                                color: Theme.of(context)
-                                                    .primaryColor,
-                                                fontSize: 32)),
-                                        TextSpan(text: ' 명이 참여했습니다')
-                                      ])),
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Column(
-                                          children: [
-                                            Icon(
-                                              Icons.favorite,
-                                              color: Colors.pinkAccent,
-                                            ),
-                                            Text('7423개',
-                                                style: TextStyle(
-                                                    fontSize: 10,
-                                                    color: Colors.pinkAccent)),
-                                          ],
-                                        ),
-                                        Column(children: [
-                                          SizedBox(
-                                            width: 50,
-                                            height: 50,
-                                            child: PieChart(PieChartData(
-                                                centerSpaceRadius: 12,
-                                                sectionsSpace: 0,
-                                                sections: [
-                                                  PieChartSectionData(
-                                                      radius: 12,
-                                                      value: 67,
-                                                      color: Colors.blueAccent,
-                                                      showTitle: false),
-                                                  PieChartSectionData(
-                                                      radius: 12,
-                                                      value: 33,
-                                                      color:
-                                                          Colors.grey.shade300,
-                                                      showTitle: false)
-                                                ])),
-                                          ),
-                                          Text('67%',
-                                              style: TextStyle(
-                                                  fontSize: 10,
-                                                  color: Colors.blueAccent))
-                                        ]),
-                                        Column(
-                                          children: [
-                                            Icon(
-                                              Icons.people,
-                                              color: Colors.greenAccent,
-                                            ),
-                                            Text('57832명',
-                                                style: TextStyle(
-                                                    fontSize: 10,
-                                                    color: Colors.greenAccent)),
-                                          ],
-                                        )
-                                      ])
-                                ]),
-                            margin: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black26,
-                                    offset: Offset(1.5, 1.5),
-                                    blurRadius: 2,
-                                    spreadRadius: 0,
-                                  )
-                                ],
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          SizedBox(height: 20),
-                          Container(
-                            height: 150,
-                            padding: const EdgeInsets.all(20),
-                            width: size.width,
-                            margin: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black26,
-                                    offset: Offset(1.5, 1.5),
-                                    blurRadius: 2,
-                                    spreadRadius: 0,
-                                  )
-                                ],
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          SizedBox(height: 20),
-                          Container(
-                            height: 150,
-                            padding: const EdgeInsets.all(20),
-                            width: size.width,
-                            margin: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black26,
-                                    offset: Offset(1.5, 1.5),
-                                    blurRadius: 2,
-                                    spreadRadius: 0,
-                                  )
-                                ],
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                          SizedBox(height: 20),
-                          Container(
-                            height: 150,
-                            padding: const EdgeInsets.all(20),
-                            width: size.width,
-                            margin: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black26,
-                                    offset: Offset(1.5, 1.5),
-                                    blurRadius: 2,
-                                    spreadRadius: 0,
-                                  )
-                                ],
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
+                          ParticipationReport(
+                              size: size, eventReport: eventReport),
+                          ExposureReport(size: size, eventReport: eventReport),
+                          ExpenditureReport(
+                              size: size, eventReport: eventReport),
                         ],
                       ),
                     ),
