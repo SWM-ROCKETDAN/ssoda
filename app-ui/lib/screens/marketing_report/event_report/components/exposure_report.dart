1932a3e93934d26c28f9b211118f490d285bfac9
yjyoon-dev <yjyoon.dev@gmail.com>
2021-07-23 17:24:04 +0900

    draw line chart on exposure report card

--- app-ui/lib/screens/marketing_report/event_report/components/exposure_report.dart
+++ app-ui/lib/screens/marketing_report/event_report/components/exposure_report.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:hashchecker/constants.dart';
 import 'package:hashchecker/models/event_report.dart';
@@ -18,6 +19,11 @@ class ExposureReport extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Color> gradientColors = [
+      const Color(0xff23b6e6),
+      const Color(0xff02d39a),
+    ];
+
     return Container(
       padding: const EdgeInsets.all(20),
       width: size.width,
@@ -70,42 +76,140 @@ class ExposureReport extends StatelessWidget {
           SizedBox(height: kDefaultPadding),
           SizedBox(
             width: size.width,
-            child: Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Column(children: [
-                  Icon(
-                    Icons.person_outline,
-                    size: 36,
-                  ),
-                  Row(
-                    children: [
-                      Text('1',
-                          style: TextStyle(
+                Icon(
+                  Icons.person_outline,
+                  size: 48,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('1',
+                        style: TextStyle(
+                          color: kThemeColor,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text('인 노출 당 ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 14)),
+                    NumberSlideAnimation(
+                        number: (eventReport.costSum ~/ eventReport.exposeCount)
+                            .toString(),
+                        duration: const Duration(seconds: 3),
+                        curve: Curves.easeOut,
+                        textStyle: TextStyle(
                             color: kThemeColor,
                             fontSize: 28,
+                            fontWeight: FontWeight.bold),
+                        format: NumberFormatMode.comma),
+                    Text('원 사용',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 14))
+                  ],
+                ),
+                Container(
+                  width: size.width * 0.7,
+                  height: 120,
+                  child: LineChart(LineChartData(
+                    gridData: FlGridData(
+                      show: false,
+                      drawVerticalLine: true,
+                      getDrawingHorizontalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xff37434d),
+                          strokeWidth: 1,
+                        );
+                      },
+                      getDrawingVerticalLine: (value) {
+                        return FlLine(
+                          color: const Color(0xff37434d),
+                          strokeWidth: 1,
+                        );
+                      },
+                    ),
+                    titlesData: FlTitlesData(
+                      show: false,
+                      bottomTitles: SideTitles(
+                        showTitles: false,
+                        reservedSize: 22,
+                        getTextStyles: (value) => const TextStyle(
+                            color: Color(0xff68737d),
                             fontWeight: FontWeight.bold,
-                          )),
-                      Text('인 노출 당 ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 14)),
-                      NumberSlideAnimation(
-                          number:
-                              (eventReport.costSum ~/ eventReport.exposeCount)
-                                  .toString(),
-                          duration: const Duration(seconds: 3),
-                          curve: Curves.easeOut,
-                          textStyle: TextStyle(
-                              color: kThemeColor,
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold),
-                          format: NumberFormatMode.comma),
-                      Text('원 사용',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 14))
+                            fontSize: 16),
+                        getTitles: (value) {
+                          switch (value.toInt()) {
+                            case 2:
+                              return 'MAR';
+                            case 5:
+                              return 'JUN';
+                            case 8:
+                              return 'SEP';
+                          }
+                          return '';
+                        },
+                        margin: 8,
+                      ),
+                      leftTitles: SideTitles(
+                        showTitles: false,
+                        getTextStyles: (value) => const TextStyle(
+                          color: Color(0xff67727d),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                        getTitles: (value) {
+                          switch (value.toInt()) {
+                            case 1:
+                              return '10k';
+                            case 3:
+                              return '30k';
+                            case 5:
+                              return '50k';
+                          }
+                          return '';
+                        },
+                        reservedSize: 28,
+                        margin: 12,
+                      ),
+                    ),
+                    borderData: FlBorderData(
+                        show: true,
+                        border: Border.all(
+                            color: const Color(0xff37434d), width: 0)),
+                    minX: 0,
+                    maxX: 11,
+                    minY: 0,
+                    maxY: 6,
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: [
+                          FlSpot(0, 3),
+                          FlSpot(2.6, 2),
+                          FlSpot(4.9, 5),
+                          FlSpot(6.8, 3.1),
+                          FlSpot(8, 4),
+                          FlSpot(9.5, 3),
+                          FlSpot(11, 4),
+                        ],
+                        isCurved: true,
+                        colors: gradientColors,
+                        barWidth: 5,
+                        isStrokeCapRound: true,
+                        dotData: FlDotData(
+                          show: false,
+                        ),
+                        belowBarData: BarAreaData(
+                          show: true,
+                          colors: gradientColors
+                              .map((color) => color.withOpacity(0.3))
+                              .toList(),
+                        ),
+                      ),
                     ],
-                  )
-                ]),
+                  )),
+                )
               ],
             ),
           ),
