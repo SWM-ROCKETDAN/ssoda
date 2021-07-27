273c9bf3ca6d9b4b99c204f6659e6fd3687da43d
unknown <yjyoon.dev@gmail.com>
2021-07-27 19:32:07 +0900

    implement naver login through flutter sdk

--- app-ui/lib/screens/sign_in/components/naver_sign_in_button.dart
+++ app-ui/lib/screens/sign_in/components/naver_sign_in_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class NaverSignInButton extends StatefulWidget {
   const NaverSignInButton({
@@ -13,20 +14,16 @@ class NaverSignInButton extends StatefulWidget {
 }
 
 class _NaverSignInButtonState extends State<NaverSignInButton> {
+  final _url =
+      'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/naver';
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: widget.size.width,
       child: TextButton(
-          onPressed: () async {
-            final response = await http.get(Uri.parse(
-                'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/naver'));
-
-            if (response.statusCode == 200) {
-              print(response.body);
-            } else
-              print('error');
-          },
+          onPressed: () async => await canLaunch(_url)
+              ? await launch(_url)
+              : throw 'Could not launch $_url',
           child: Row(children: [
             Image.asset(
               'assets/images/sign_in/naver_logo.png',
--- app-ui/lib/screens/sign_in/sign_in_screen.dart
+++ app-ui/lib/screens/sign_in/sign_in_screen.dart
@@ -5,6 +5,11 @@ import 'package:hashchecker/constants.dart';
 import 'components/kakao_sign_in_button.dart';
 import 'components/naver_sign_in_button.dart';
 
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
+
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -13,41 +18,130 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  bool isLogin = false;
+
+  String? accesToken;
+
+  String? expiresAt;
+
+  String? tokenType;
+
+  String? name;
+
+  String? refreshToken;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
-          child: Column(children: [
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DefaultTextStyle(
-                  style: TextStyle(
-                      fontSize: 72,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87),
-                  child: AnimatedTextKit(
-                    animatedTexts: [
-                      WavyAnimatedText('Hello',
-                          speed: const Duration(milliseconds: 500)),
-                    ],
-                    isRepeatingAnimation: true,
-                  ),
-                ),
-              ],
-            )),
-            Column(
-              children: [
-                NaverSignInButton(size: size),
-                SizedBox(height: kDefaultPadding / 3 * 2),
-                KakaoSignInButton(size: size),
-              ],
-            ),
-          ])),
+      body: SafeArea(
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 30),
+            child: Column(children: [
+              Expanded(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        new Text('isLogin: $isLogin\n'),
+                        new Text('accessToken: $accesToken\n'),
+                        new Text('tokenType: $tokenType\n'),
+                        new Text('user: $name\n'),
+                      ],
+                    ),
+                    ElevatedButton(
+                        key: null,
+                        onPressed: buttonLoginPressed,
+                        child: Text(
+                          "LogIn",
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: "Roboto"),
+                        )),
+                    ElevatedButton(
+                        key: null,
+                        onPressed: buttonLogoutPressed,
+                        child: Text(
+                          "LogOut",
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: "Roboto"),
+                        )),
+                    ElevatedButton(
+                        key: null,
+                        onPressed: buttonTokenPressed,
+                        child: Text(
+                          "GetToken",
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: "Roboto"),
+                        )),
+                    ElevatedButton(
+                        key: null,
+                        onPressed: buttonGetUserPressed,
+                        child: Text(
+                          "GetUser",
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: "Roboto"),
+                        ))
+                  ])),
+              Column(
+                children: [
+                  NaverSignInButton(size: size),
+                  SizedBox(height: kDefaultPadding / 3 * 2),
+                  KakaoSignInButton(size: size),
+                ],
+              ),
+            ])),
+      ),
     );
+  }
+
+  Future<void> buttonLoginPressed() async {
+    NaverLoginResult res = await FlutterNaverLogin.logIn();
+    setState(() {
+      name = res.account.nickname;
+      isLogin = true;
+    });
+    buttonTokenPressed();
+    buttonGetUserPressed();
+  }
+
+  Future<void> buttonTokenPressed() async {
+    NaverAccessToken res = await FlutterNaverLogin.currentAccessToken;
+    setState(() {
+      accesToken = res.accessToken;
+      tokenType = res.tokenType;
+    });
+  }
+
+  Future<void> buttonLogoutPressed() async {
+    FlutterNaverLogin.logOut();
+    setState(() {
+      isLogin = false;
+      accesToken = null;
+      tokenType = null;
+      name = null;
+    });
+  }
+
+  Future<void> buttonGetUserPressed() async {
+    NaverAccountResult res = await FlutterNaverLogin.currentAccount();
+    setState(() {
+      name = res.name;
+    });
   }
 }
