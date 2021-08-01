730d62f441c2aa6e7b26c8fa9788fdccbd7b3698
iamhge <fine632@gmail.com>
2021-08-01 23:32:47 +0900

    duplicate check of join user

--- service-server/src/main/java/com/rocketdan/serviceserver/service/JoinUserService.java
+++ service-server/src/main/java/com/rocketdan/serviceserver/service/JoinUserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +20,13 @@ public class JoinUserService {
     @Transactional
     public Long save(Long joinPostId) {
         JoinPost linkedJoinPost = joinPostRepository.findById(joinPostId).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + joinPostId));
+
+        // 같은 snsId, type을 가진 join user 가 존재할 경우 저장하지 않고 리턴
+        Optional<JoinUser> joinUser = joinUserRepository.findBySnsIdAndType(linkedJoinPost.getSnsId(), linkedJoinPost.getType());
+        if (joinUser.isPresent()) {
+            return joinUser.get().getId();
+        }
+
         JoinUser savedJoinUser = JoinUser.builder()
                 .snsId(linkedJoinPost.getSnsId())
                 .type(linkedJoinPost.getType())
