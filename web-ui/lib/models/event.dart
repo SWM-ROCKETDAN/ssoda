55b45b6c660c15268aa3e1bea1bc69c7533568c6
unknown <yjyoon.dev@gmail.com>
2021-08-02 04:28:38 +0900

    copy screen / pub / assets

---
+++ web-ui/lib/models/event.dart
@@ -0,0 +1,32 @@
+import 'reward.dart';
+import 'period.dart';
+import 'template.dart';
+
+enum EventStatus { WAITING, PROCEEDING, ENDED }
+
+class Event {
+  final String title;
+  final List<Reward?> rewardList;
+  final Period period;
+  final List<String?> images;
+  final List<String> hashtagList;
+  final List<bool> requireList;
+  final Template template;
+
+  String get getTitle => title;
+  List<Reward?> get getRewardList => rewardList;
+  List<String> get getHashtagList => hashtagList;
+  Period get getPeriod => period;
+  List<String?> get getImages => images;
+  List<bool> get getRequireList => requireList;
+  Template get getTemplate => template;
+
+  Event(
+      {required this.title,
+      required this.rewardList,
+      required this.hashtagList,
+      required this.period,
+      required this.images,
+      required this.requireList,
+      required this.template});
+}
---
+++ web-ui/lib/screens/event_join/components/event_rewards.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:hashchecker_web/constants.dart';
+import 'package:hashchecker_web/models/event.dart';
+import 'dart:io';
+
+class EventRewards extends StatelessWidget {
+  const EventRewards({
+    Key? key,
+    required this.event,
+  }) : super(key: key);
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text('이벤트 상품',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17)),
+      SizedBox(height: kDefaultPadding),
+      SizedBox(
+        height: 116,
+        child: ListView.separated(
+          itemBuilder: (context, index) => ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              event.rewardList[index]!.imgPath,
+              fit: BoxFit.cover,
+              width: 100,
+              height: 110,
+            ),
+          ),
+          itemCount: event.rewardList.length,
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.fromLTRB(3, 6, 3, 6),
+          separatorBuilder: (context, index) => SizedBox(width: 12),
+        ),
+      )
+    ]);
+  }
+}
