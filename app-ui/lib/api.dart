b4014ec6e6f4fa1b6c050b13d726bb6a5fb28be7
unknown <yjyoon.dev@gmail.com>
2021-08-04 01:14:37 +0900

    add event join url constant

--- app-ui/lib/api.dart
+++ app-ui/lib/api.dart
@@ -1,6 +1,8 @@
 const baseUrl =
     'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/api/v1';
 
+const eventJoinUrl = 'http://3.36.105.82';
+
 enum API { LOGIN, GET_ALL_EVENTS, CREATE_EVENT }
 
 Map<API, String> apiMap = {
