1bfb12141b110ffcdf41fe7a5ad79efb2a384f3d
unknown <yjyoon.dev@gmail.com>
2021-08-04 04:20:02 +0900

    add api.dart

---
+++ web-ui/lib/api.dart
@@ -0,0 +1,13 @@
+const baseUrl =
+    'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/api/v1/';
+
+enum API { GET_EVENT }
+
+Map<API, String> apiMap = {API.GET_EVENT: '/events'};
+
+String getApi(API apiType, {String? parameter}) {
+  String api = baseUrl;
+  api += apiMap[apiType]!;
+  if (parameter != null) api += '/$parameter';
+  return api;
+}
