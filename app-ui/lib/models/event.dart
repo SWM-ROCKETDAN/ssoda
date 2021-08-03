0fce9540c121e46a386b36c86e867c76f7defc54
unknown <yjyoon.dev@gmail.com>
2021-08-03 17:15:41 +0900

    apply json serialization to event models

--- app-ui/lib/models/event.dart
+++ app-ui/lib/models/event.dart
@@ -1,6 +1,7 @@
 import 'reward.dart';
 import 'period.dart';
 import 'template.dart';
+import 'package:intl/intl.dart';
 
 enum EventStatus { WAITING, PROCEEDING, ENDED }
 
@@ -29,4 +30,30 @@ class Event {
       required this.images,
       required this.requireList,
       required this.template});
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'startDate': DateFormat('yyyy-MM-ddTHH:mm:ss').format(period.startDate),
+        'finishDate': period.finishDate == null
+            ? null
+            : DateFormat('yyyy-MM-ddTHH:mm:ss').format(period.finishDate!),
+        'images': images,
+        'rewards': rewardList,
+        'hashtags': hashtagList,
+        'requirements': requireList,
+        'template': template.id
+      };
+
+  Event.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        rewardList = json['rewards'],
+        period = Period(
+            DateFormat('yyyy-MM-ddTHH:mm:ss').parse(json['startDate']),
+            json['finishDate'] == null
+                ? null
+                : DateFormat('yyyy-MM-ddTHH:mm:ss').parse(json['finishDate'])),
+        images = json['images'],
+        hashtagList = json['hashtags'],
+        requireList = json['requirements'],
+        template = Template(json['template']);
 }
--- app-ui/lib/models/period.dart
+++ app-ui/lib/models/period.dart
@@ -1,15 +1,12 @@
 class Period {
   DateTime _startDate;
   DateTime? _finishDate;
-  int _dateShortcut;
 
   DateTime get startDate => _startDate;
   DateTime? get finishDate => _finishDate;
-  int get dateShortcut => _dateShortcut;
 
   set startDate(DateTime startDate) => _startDate = startDate;
   set finishDate(DateTime? finishDate) => _finishDate = finishDate;
-  set dateShortcut(int dateShortcut) => _dateShortcut = dateShortcut;
 
-  Period(this._startDate, this._finishDate, this._dateShortcut);
+  Period(this._startDate, this._finishDate);
 }
