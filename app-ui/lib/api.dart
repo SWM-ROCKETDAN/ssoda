fb4e69e6c0f149de3be6263e0317fe21027391ad
unknown <yjyoon.dev@gmail.com>
2021-08-03 17:34:58 +0900

    add create event api

--- app-ui/lib/api.dart
+++ app-ui/lib/api.dart
@@ -1,9 +1,13 @@
 const baseUrl =
     'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/api/v1';
 
-enum API { LOGIN, GET_ALL_EVENTS }
+enum API { LOGIN, GET_ALL_EVENTS, CREATE_EVENT }
 
-Map<API, String> apiMap = {API.LOGIN: '/login', API.GET_ALL_EVENTS: '/events'};
+Map<API, String> apiMap = {
+  API.LOGIN: '/login',
+  API.GET_ALL_EVENTS: '/events',
+  API.CREATE_EVENT: '/events/hashtag/stores'
+};
 
 String getApi(API apiType, {String? parameter}) {
   String api = baseUrl;
