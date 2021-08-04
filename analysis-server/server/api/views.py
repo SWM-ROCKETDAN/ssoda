986ef5bc97afdcff4f90ff8f94c392c0381a979b
송영모 <mooyoung2309@gmail.com>
2021-08-04 15:25:48 +0900

    docs: get reward level

--- analysis-server/server/api/views.py
+++ analysis-server/server/api/views.py
@@ -28,7 +28,7 @@ class JoinPostView(APIView):
             raise status.HTTP_404_NOT_FOUND
 
     # GET 요청 -> post 크롤링 -> join_post 업데이트
-    def get(self, request, pk, formate=None):
+    def put(self, request, pk, formate=None):
         join_post = self.get_object(pk)
         join_post_serializer = JoinPostSerializer(join_post)
         join_post_url = join_post_serializer.data.get('url')
@@ -65,7 +65,7 @@ class JoinUserView(APIView):
             raise status.HTTP_404_NOT_FOUND
 
     # GET 요청 -> user 크롤링 -> join_user 업데이트
-    def get(self, request, pk, formate=None):
+    def put(self, request, pk, formate=None):
         join_user = self.get_object(pk)
         join_user_serializer = JoinUserSerializer(join_user)
         join_user_url = join_user_serializer.data.get('sns_id')
@@ -99,9 +99,8 @@ class JoinRewardView(APIView):
         join_collection_serializer = JoinCollectionSerializer(data=join_collection, many=True)
         join_collection_serializer.is_valid()
         join_reward = JoinReward(join_collection_serializer.data, pk)
-        join_reward.get_reward_rate_list()
-        join_reward.get_reward_point_rate()
-        return Response(join_collection_serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        reward_level = join_reward.get_reward_level()
+        return Response({'reward_level': reward_level}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TestView(APIView):
