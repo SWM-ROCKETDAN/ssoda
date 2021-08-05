30606c31c1df016d9c31e15259c8047c6dd16592
unknown <yjyoon.dev@gmail.com>
2021-08-06 04:22:02 +0900

    change color

--- app-ui/lib/constants.dart
+++ app-ui/lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kThemeColor = Color(0xFF156fe9);
+const kThemeColor = Color(0xFF155be9);
 const kDefaultPadding = 15.0;
 const kDefaultNumberSliderDuration = Duration(seconds: 2);
