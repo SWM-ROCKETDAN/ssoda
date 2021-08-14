d068565835bd4f672f64442c4c4c9fe126675505
unknown <yjyoon.dev@gmail.com>
2021-08-14 14:39:46 +0900

    define new default colors

--- app-ui/lib/constants.dart
+++ app-ui/lib/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kThemeColor = Color(0xFF155be9);
+const kThemeColor = Color(0xFF2172e2);
+const kScaffoldBackgroundColor = Color(0xFFf8fbff);
+final kShadowColor = Color(0xFF90a0b8).withOpacity(0.3);
+const kDefaultFontColor = Color(0xFF001024);
+const kLiteFontColor = Color(0xFF808893);
 const kDefaultPadding = 15.0;
 const kDefaultNumberSliderDuration = Duration(seconds: 2);
