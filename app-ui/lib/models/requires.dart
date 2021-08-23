bcc926f49404ecb6821702580e1cbef34361236b
yjyoon-dev <yjyoon.dev@gmail.com>
2021-08-23 21:10:39 +0900

    define singleLineStringRequireList

--- app-ui/lib/models/requires.dart
+++ app-ui/lib/models/requires.dart
@@ -9,6 +9,15 @@ const requireStringList = [
   '전체 공개로\n게시하기'
 ];
 
+const requireSingleLineStringList = [
+  '매장 계정 팔로우하기',
+  '가게 사진 올리기',
+  '메뉴 사진 올리기',
+  '위치 정보 첨부하기',
+  '가격 정보 기입하기',
+  '전체 공개로 게시하기'
+];
+
 const requireIconList = [
   Icons.person_add_rounded,
   Icons.store_rounded,
