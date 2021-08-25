ae8476802681fcb3fa8edbfbd8954fe1cc3082db
yjyoon-dev <yjyoon.dev@gmail.com>
2021-08-25 14:33:09 +0900

    apply event thumbnail animation

--- app-ui/lib/screens/event_list/components/event_list_tile.dart
+++ app-ui/lib/screens/event_list/components/event_list_tile.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:hashchecker/constants.dart';
@@ -31,165 +32,131 @@ class EventListTile extends StatelessWidget {
       children: [
         Container(
             height: 114,
-            child: Stack(
-              children: [
-                Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                        width: size.width * 0.7,
-                        height: 100,
-                        child: Material(
-                          color: Colors.white.withOpacity(0.0),
-                          child: InkWell(
-                            highlightColor: kShadowColor,
-                            overlayColor:
-                                MaterialStateProperty.all<Color>(kShadowColor),
-                            borderRadius: BorderRadius.circular(12),
-                            onTap: () {
-                              Navigator.push(
+            child: Stack(children: [
+              Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                      width: size.width * 0.7,
+                      height: 100,
+                      child: Material(
+                        color: Colors.white.withOpacity(0.0),
+                        child: InkWell(
+                          highlightColor: kShadowColor,
+                          overlayColor:
+                              MaterialStateProperty.all<Color>(kShadowColor),
+                          borderRadius: BorderRadius.circular(12),
+                          onTap: () {
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => EventDetailScreen(
-                                      eventListItem: eventList[index]),
+                                    builder: (context) => EventDetailScreen(
+                                        eventListItem: eventList[index])));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 10, 15, 10),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: size.width * 0.20,
                                 ),
-                              );
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 10, 15, 10),
-                              child: Row(
-                                children: [
-                                  SizedBox(
-                                    width: size.width * 0.20,
-                                  ),
-                                  Expanded(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: [
-                                        AutoSizeText(
-                                          eventList[index].title,
-                                          style: TextStyle(
-                                            color: kDefaultFontColor,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                          maxLines: 1,
-                                          minFontSize: 10,
-                                          overflow: TextOverflow.ellipsis,
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      AutoSizeText(
+                                        eventList[index].title,
+                                        style: TextStyle(
+                                          color: kDefaultFontColor,
+                                          fontWeight: FontWeight.bold,
                                         ),
-                                        SizedBox(height: kDefaultPadding / 3),
-                                        Text(
-                                            '${eventList[index].startDate} ~ ${eventList[index].finishDate}',
-                                            style: TextStyle(
-                                                color: kLiteFontColor,
-                                                fontSize: 12)),
-                                        SizedBox(height: kDefaultPadding),
-                                        IntrinsicHeight(
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.end,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              Container(
+                                        maxLines: 1,
+                                        minFontSize: 10,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      SizedBox(height: kDefaultPadding / 3),
+                                      Text(
+                                          '${eventList[index].startDate} ~ ${eventList[index].finishDate}',
+                                          style: TextStyle(
+                                              color: kLiteFontColor,
+                                              fontSize: 12)),
+                                      SizedBox(height: kDefaultPadding),
+                                      IntrinsicHeight(
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      0, 4, 8, 4),
+                                              child: Text(
+                                                  _statusStringMap[
+                                                      eventList[index].status]!,
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: _statusColorMap[
+                                                          eventList[index]
+                                                              .status],
+                                                      fontWeight: eventList[
+                                                                      index]
+                                                                  .status ==
+                                                              EventStatus
+                                                                  .PROCEEDING
+                                                          ? FontWeight.bold
+                                                          : FontWeight.normal)),
+                                            ),
+                                            VerticalDivider(
+                                                width: 0,
+                                                color: kShadowColor
+                                                    .withOpacity(0.6)),
+                                            GestureDetector(
+                                              onTap: () =>
+                                                  showBarModalBottomSheet(
+                                                expand: true,
+                                                context: context,
+                                                builder: (context) =>
+                                                    EventEditModal(),
+                                              ),
+                                              child: Container(
                                                 padding:
                                                     const EdgeInsets.fromLTRB(
-                                                        0, 4, 8, 4),
-                                                child: Text(
-                                                    _statusStringMap[
-                                                        eventList[index]
-                                                            .status]!,
-                                                    style: TextStyle(
-                                                        fontSize: 12,
-                                                        color: _statusColorMap[
-                                                            eventList[index]
-                                                                .status],
-                                                        fontWeight: eventList[
-                                                                        index]
-                                                                    .status ==
-                                                                EventStatus
-                                                                    .PROCEEDING
-                                                            ? FontWeight.bold
-                                                            : FontWeight
-                                                                .normal)),
+                                                        8, 4, 8, 4),
+                                                child: Icon(Icons.edit_rounded,
+                                                    color: Colors.blueGrey,
+                                                    size: 18),
                                               ),
-                                              VerticalDivider(
-                                                  width: 0,
-                                                  color: kShadowColor
-                                                      .withOpacity(0.6)),
-                                              GestureDetector(
-                                                onTap: () =>
-                                                    showBarModalBottomSheet(
-                                                  expand: true,
-                                                  context: context,
-                                                  builder: (context) =>
-                                                      EventEditModal(),
-                                                ),
-                                                child: Container(
-                                                  padding:
-                                                      const EdgeInsets.fromLTRB(
-                                                          8, 4, 8, 4),
-                                                  child: Icon(
-                                                      Icons.edit_rounded,
-                                                      color: Colors.blueGrey,
-                                                      size: 18),
-                                                ),
+                                            ),
+                                            VerticalDivider(
+                                                width: 0,
+                                                color: kShadowColor
+                                                    .withOpacity(0.6)),
+                                            GestureDetector(
+                                              onTap: () {
+                                                showEventDeleteDialog(context);
+                                              },
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.fromLTRB(
+                                                        8, 4, 0, 4),
+                                                child: Icon(
+                                                    Icons.delete_rounded,
+                                                    color: Colors.blueGrey,
+                                                    size: 18),
                                               ),
-                                              VerticalDivider(
-                                                  width: 0,
-                                                  color: kShadowColor
-                                                      .withOpacity(0.6)),
-                                              GestureDetector(
-                                                onTap: () {
-                                                  showEventDeleteDialog(
-                                                      context);
-                                                },
-                                                child: Container(
-                                                  padding:
-                                                      const EdgeInsets.fromLTRB(
-                                                          8, 4, 0, 4),
-                                                  child: Icon(
-                                                      Icons.delete_rounded,
-                                                      color: Colors.blueGrey,
-                                                      size: 18),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: kShadowColor,
-                                spreadRadius: 6,
-                                blurRadius: 20,
-                                offset:
-                                    Offset(0, 0), // changes position of shadow
-                              ),
-                            ],
-                            color: kScaffoldBackgroundColor,
-                            borderRadius: BorderRadius.circular(12)))),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Container(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(12)),
-                        child: Image.asset(
-                          eventList[index].thumbnail,
-                          width: size.width * 0.37,
-                          height: 100,
-                          fit: BoxFit.cover,
                         ),
                       ),
                       decoration: BoxDecoration(
@@ -203,10 +170,46 @@ class EventListTile extends StatelessWidget {
                             ),
                           ],
                           color: kScaffoldBackgroundColor,
-                          borderRadius: BorderRadius.circular(12))),
-                ),
-              ],
-            )),
+                          borderRadius: BorderRadius.circular(12)))),
+              Positioned(
+                  top: 0,
+                  left: 0,
+                  child: OpenContainer<bool>(
+                    openColor: kScaffoldBackgroundColor,
+                    openElevation: 0,
+                    openShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                    closedColor: kScaffoldBackgroundColor,
+                    closedShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                    closedElevation: 0,
+                    transitionType: ContainerTransitionType.fade,
+                    openBuilder: (context, _) =>
+                        EventDetailScreen(eventListItem: eventList[index]),
+                    closedBuilder: (context, openContainer) => Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                          child: Image.asset(
+                            eventList[index].thumbnail,
+                            width: size.width * 0.37,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: kShadowColor,
+                                spreadRadius: 6,
+                                blurRadius: 20,
+                                offset:
+                                    Offset(0, 0), // changes position of shadow
+                              ),
+                            ],
+                            color: kScaffoldBackgroundColor,
+                            borderRadius: BorderRadius.circular(12))),
+                  )),
+            ])),
         SizedBox(height: kDefaultPadding)
       ],
     );
