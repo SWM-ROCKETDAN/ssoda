6ee2eb6cca454685ce202d7440501aa308baa896
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-08-30 04:01:46 +0900

    add login api

--- app-ui/lib/api.dart
+++ app-ui/lib/api.dart
@@ -1,16 +1,23 @@
 const baseUrl =
-    'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080/api/v1';
+    'http://ec2-3-37-85-236.ap-northeast-2.compute.amazonaws.com:8080';
 
 const eventJoinUrl =
     'http://ec2-13-124-246-123.ap-northeast-2.compute.amazonaws.com';
 
-enum API { LOGIN, GET_ALL_EVENTS, CREATE_EVENT, CREATE_REWARDS }
+enum API {
+  NAVER_LOGIN,
+  KAKAO_LOGIN,
+  GET_ALL_EVENTS,
+  CREATE_EVENT,
+  CREATE_REWARDS
+}
 
 Map<API, String> apiMap = {
-  API.LOGIN: '/login',
-  API.GET_ALL_EVENTS: '/events',
-  API.CREATE_EVENT: '/events/hashtag/stores',
-  API.CREATE_REWARDS: '/rewards/events'
+  API.NAVER_LOGIN: '/oauth2/authorization/naver',
+  API.KAKAO_LOGIN: '/oauth2/authorization/kakao',
+  API.GET_ALL_EVENTS: '/api/v1/events',
+  API.CREATE_EVENT: '/api/v1/events/hashtag/stores',
+  API.CREATE_REWARDS: '/api/v1/rewards/events'
 };
 
 String getApi(API apiType, {String? parameter}) {
