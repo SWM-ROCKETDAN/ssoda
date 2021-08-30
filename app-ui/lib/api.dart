1e2d36017ad1adf27bf6fd90d2937b83c2a84f93
yjyoon-dev <yjyoon.dev@gmail.com>
2021-08-30 16:56:51 +0900

    add updated apis

--- app-ui/lib/api.dart
+++ app-ui/lib/api.dart
@@ -7,7 +7,10 @@ const eventJoinUrl =
 enum API {
   NAVER_LOGIN,
   KAKAO_LOGIN,
+  GET_USER_INFO,
+  GET_USER_STORE,
   GET_ALL_EVENTS,
+  CREATE_STORE,
   CREATE_EVENT,
   CREATE_REWARDS
 }
@@ -15,7 +18,10 @@ enum API {
 Map<API, String> apiMap = {
   API.NAVER_LOGIN: '/oauth2/authorization/naver',
   API.KAKAO_LOGIN: '/oauth2/authorization/kakao',
+  API.GET_USER_INFO: '/api/v1/users/me',
+  API.GET_USER_STORE: '/api/v1/users/me/stores',
   API.GET_ALL_EVENTS: '/api/v1/events',
+  API.CREATE_STORE: '/api/v1/stores/users',
   API.CREATE_EVENT: '/api/v1/events/hashtag/stores',
   API.CREATE_REWARDS: '/api/v1/rewards/events'
 };
