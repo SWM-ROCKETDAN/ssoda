838fa858cb5bb19b2b3022acfd5cb1e8580c824c
iamhge <fine632@gmail.com>
2021-09-11 23:17:55 +0900

    StoreListResponseDto (imagePaths -> logoImagePath)

--- service-server/src/main/java/com/rocketdan/serviceserver/app/dto/store/StoreListResponseDto.java
+++ service-server/src/main/java/com/rocketdan/serviceserver/app/dto/store/StoreListResponseDto.java
@@ -3,17 +3,15 @@ package com.rocketdan.serviceserver.app.dto.store;
 import com.rocketdan.serviceserver.domain.store.Store;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public class StoreListResponseDto {
     private Long id;
     private String name;
-    private List<String> imagePaths;
+    private String logoImagePath;
 
     public StoreListResponseDto(Store entity) {
         this.id = entity.getId();
         this.name = entity.getName();
-        this.imagePaths = List.copyOf(entity.getImagePaths());
+        this.logoImagePath = entity.getLogoImagePath();
     }
 }
