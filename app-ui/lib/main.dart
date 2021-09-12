56d94fde8ed9458d8d09a3f206ffe1755c5b8811
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-09-13 04:10:59 +0900

    implement navigating when app started

--- app-ui/lib/main.dart
+++ app-ui/lib/main.dart
@@ -2,28 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hashchecker/constants.dart';
+import 'package:hashchecker/models/selected_store.dart';
 import 'package:hashchecker/screens/create_store/create_store_screen.dart';
 import 'package:hashchecker/screens/event_list/event_list_screen.dart';
 import 'package:hashchecker/screens/hall/hall_screen.dart';
 import 'package:hashchecker/screens/on_boarding/on_boarding_screen.dart';
 import 'package:hashchecker/screens/sign_in/sign_in_screen.dart';
 import 'package:hashchecker/screens/splash/splash_screen.dart';
-
+import 'package:provider/provider.dart';
 import 'screens/create_store/components/intro.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness: Brightness.dark));
-  runApp(MyApp());
+  runApp(Provider(create: (_) => SelectedStore(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Init.instance.initialize(),
+      future: Init.instance.initialize(context),
       builder: (context, AsyncSnapshot snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return MaterialApp(home: SplashScreen());
@@ -44,7 +47,7 @@ class MyApp extends StatelessWidget {
                   visualDensity: VisualDensity.adaptivePlatformDensity,
                   scaffoldBackgroundColor: kScaffoldBackgroundColor,
                   accentColor: kShadowColor),
-              home: OnBoardingScreen());
+              home: snapshot.data);
         }
       },
     );
@@ -75,7 +78,30 @@ class Init {
   Init._();
   static final instance = Init._();
 
-  Future initialize() async {
-    await Future.delayed(Duration(milliseconds: 2500));
+  Future<Widget?> initialize(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // on first launching
+    final checkFirst = prefs.getBool('checkFirst');
+    if (checkFirst == null) return OnBoardingScreen();
+
+    final storage = new FlutterSecureStorage();
+
+    final accessToken = await storage.read(key: 'ACCESS_TOKEN');
+    final refreshToken = await storage.read(key: 'REFRESH_TOKEN');
+
+    // on not login yet
+    if (accessToken == null || refreshToken == null) return SignInScreen();
+
+    // on empty store
+    final selectedStore = prefs.getInt('selectedStore');
+    if (selectedStore == null) return CreateStoreIntroScreen();
+
+    context.read<SelectedStore>().id = selectedStore;
+
+    // on default
+    return HallScreen();
+
+    //await Future.delayed(Duration(milliseconds: 2500));
   }
 }
