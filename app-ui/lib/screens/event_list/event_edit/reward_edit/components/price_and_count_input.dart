5f9541b3a4e531ed82e15a4f8494b5267e75fe99
yjyoon-dev <yjyoon.dev@gmail.com>
2021-09-13 23:29:12 +0900

    implement reward edit screen

---
+++ app-ui/lib/screens/event_list/event_edit/reward_edit/components/price_and_count_input.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:hashchecker/constants.dart';
+
+class PriceAndCountInput extends StatelessWidget {
+  const PriceAndCountInput({
+    Key? key,
+    required TextEditingController priceController,
+    required TextEditingController countController,
+  })  : _priceController = priceController,
+        _countController = countController,
+        super(key: key);
+
+  final TextEditingController _priceController;
+  final TextEditingController _countController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: TextField(
+                textAlign: TextAlign.end,
+                controller: _priceController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.monetization_on_outlined),
+                    labelText: '단가',
+                    suffixText: '원'),
+              ),
+            ),
+            SizedBox(height: kDefaultPadding),
+            Expanded(
+              child: TextField(
+                  textAlign: TextAlign.end,
+                  controller: _countController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.toll_outlined),
+                      labelText: '수량',
+                      suffixText: '개')),
+            ),
+          ],
+        ));
+  }
+}
