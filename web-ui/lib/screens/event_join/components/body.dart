aa8dc105a7026ee2c5eae32174527e617b4399c3
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-09-30 03:58:36 +0900

    implement roulette

--- web-ui/lib/screens/event_join/components/body.dart
+++ web-ui/lib/screens/event_join/components/body.dart
@@ -1,9 +1,9 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hashchecker_web/constants.dart';
-import 'package:hashchecker_web/models/event.dart';
 import 'package:hashchecker_web/screens/event_join/components/event_join_with_url.dart';
+import 'package:hashchecker_web/screens/event_join/components/loading.dart';
+import 'package:hashchecker_web/screens/event_join/components/roulette.dart';
 import 'package:loading_overlay/loading_overlay.dart';
 
 import 'event_description.dart';
@@ -31,122 +31,73 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   bool _isLoading = false;
+  int _rouletteValue = -1;
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
     SystemChrome.setApplicationSwitcherDescription(
         ApplicationSwitcherDescription(label: widget.event.title));
 
     return LoadingOverlay(
-      child: SingleChildScrollView(
-          child: Column(
-        children: [
-          HeaderWithImages(storeId: widget.storeId, event: widget.event),
-          SizedBox(height: kDefaultPadding / 4 * 3),
-          EventTitle(event: widget.event),
-          SizedBox(height: kDefaultPadding),
-          EventDescription(event: widget.event),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Divider(
-                  height: kDefaultPadding * 2,
-                  color: kShadowColor,
-                  thickness: 1.2),
-              EventRewards(event: widget.event),
-              Divider(
-                  height: kDefaultPadding * 2,
-                  color: kShadowColor,
-                  thickness: 1.2),
-              EventHashtags(event: widget.event),
-              Divider(
-                  height: kDefaultPadding * 2,
-                  color: kShadowColor,
-                  thickness: 1.2),
-              EventPeriod(event: widget.event),
-              Divider(
-                  height: kDefaultPadding * 2,
-                  color: kShadowColor,
-                  thickness: 1.2),
-              EventJoinWithUrl(
-                  event: widget.event,
-                  eventId: widget.eventId,
-                  storeId: widget.storeId,
-                  loading: _loading)
-            ]),
-          ),
-        ],
-      )),
-      isLoading: _isLoading,
-      color: Colors.black,
-      opacity: 0.8,
-      progressIndicator: Stack(children: [
-        Positioned(
-          top: size.height * 0.25 - 100,
-          left: 0,
-          right: 0,
-          child: SizedBox(
-              height: 100,
-              child: DefaultTextStyle(
-                style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-                child: AnimatedTextKit(
-                    repeatForever: true,
-                    animatedTexts: List.generate(
-                        widget.event.rewardList.length,
-                        (index) => RotateAnimatedText(
-                            widget.event.rewardList[index]!.name,
-                            textDirection: TextDirection.rtl,
-                            textAlign: TextAlign.center,
-                            duration: const Duration(milliseconds: 1000)))),
-              )),
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularProgressIndicator(
-                color: Colors.white,
-              ),
-              SizedBox(height: kDefaultPadding),
-              SizedBox(
-                height: 40,
-                child: DefaultTextStyle(
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      animatedTexts: [
-                        TyperAnimatedText('잠시만 기다려주세요...',
-                            speed: Duration(milliseconds: 200))
-                      ],
-                    )),
-              ),
-              SizedBox(height: kDefaultPadding / 3),
-              Text(
-                '과연 어떤 상품을 받게될까요?',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.8),
-                  fontSize: 14,
-                ),
-              )
-            ],
-          ),
-        )
-      ]),
-    );
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            HeaderWithImages(storeId: widget.storeId, event: widget.event),
+            SizedBox(height: kDefaultPadding / 4 * 3),
+            EventTitle(event: widget.event),
+            SizedBox(height: kDefaultPadding),
+            EventDescription(event: widget.event),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    EventJoinDivider(),
+                    EventRewards(event: widget.event),
+                    EventJoinDivider(),
+                    EventHashtags(event: widget.event),
+                    EventJoinDivider(),
+                    EventPeriod(event: widget.event),
+                    EventJoinDivider(),
+                    EventJoinWithUrl(
+                        event: widget.event,
+                        eventId: widget.eventId,
+                        storeId: widget.storeId,
+                        loading: _loading,
+                        roulette: _roulette)
+                  ]),
+            ),
+          ],
+        )),
+        isLoading: _isLoading || _rouletteValue != -1,
+        color: Colors.black,
+        opacity: 0.8,
+        progressIndicator: _rouletteValue != -1
+            ? Roulette(
+                rouletteValue: _rouletteValue,
+                rewardList: widget.event.rewardList)
+            : Loading());
   }
 
   void _loading(bool opt) {
     setState(() {
       _isLoading = opt;
     });
+  }
+
+  void _roulette(int value) {
+    setState(() {
+      _rouletteValue = value;
+    });
+  }
+}
+
+class EventJoinDivider extends StatelessWidget {
+  const EventJoinDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+        height: kDefaultPadding * 2, color: kShadowColor, thickness: 1.2);
   }
 }
---
+++ web-ui/lib/screens/event_join/components/roulette.dart
@@ -0,0 +1,64 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+
+class Roulette extends StatelessWidget {
+  final rouletteValue;
+  final rewardList;
+  const Roulette(
+      {Key? key, required this.rouletteValue, required this.rewardList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Map<int, List<Color>> rouletteColorMap = {
+      2: [Color(0xFFF15A5A), Color(0xFF2D95BF)],
+      3: [Color(0xFFF15A5A), Color(0xFF4EBA6F), Color(0xFF2D95BF)],
+      4: [
+        Color(0xFFF15A5A),
+        Color(0xFFF0C419),
+        Color(0xFF4EBA6F),
+        Color(0xFF2D95BF)
+      ],
+      5: [
+        Color(0xFFF15A5A),
+        Color(0xFFF0C419),
+        Color(0xFF4EBA6F),
+        Color(0xFF2D95BF),
+        Color(0xFF955BA5)
+      ]
+    };
+
+    return Container(
+        padding: const EdgeInsets.all(20),
+        child: FortuneWheel(
+            indicators: [
+              FortuneIndicator(
+                  alignment: Alignment.topCenter,
+                  child: TriangleIndicator(
+                    color: Colors.white.withOpacity(0.7),
+                  )),
+            ],
+            physics: NoPanPhysics(),
+            selected: Stream.value(rouletteValue),
+            items: List.generate(
+                rewardList.length,
+                (index) => FortuneItem(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: AutoSizeText(
+                        '   ${rewardList[index].name}',
+                        style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        maxLines: 1,
+                        minFontSize: 10,
+                      ),
+                    ),
+                    style: FortuneItemStyle(
+                        color: rouletteColorMap[rewardList.length]![index],
+                        borderWidth: 3,
+                        borderColor: Colors.white)))));
+  }
+}
