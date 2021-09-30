8bb286caeaa978c8dc8ed119cfa3c2ed6df0857e
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-10-01 00:12:23 +0900

    view notion brochure on root page

--- web-ui/lib/screens/ssoda_map/ssoda_map_screen.dart
+++ web-ui/lib/screens/ssoda_map/ssoda_map_screen.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hashchecker_web/constants.dart';
+import 'package:hashchecker_web/screens/store_event/store_event_screen.dart';
 
 class SsodaMapScreen extends StatelessWidget {
   const SsodaMapScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: kScaffoldBackgroundColor,
-        child: Text('SSODA',
-            style: TextStyle(
-                color: kThemeColor,
-                fontSize: 24,
-                fontWeight: FontWeight.bold)));
+    InAppWebViewController? webViewController;
+    InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+        ),
+        ios: IOSInAppWebViewOptions(
+          allowsInlineMediaPlayback: true,
+        ));
+
+    return Scaffold(
+        body: WillPopScope(
+      onWillPop: () async {
+        webViewController?.goBack();
+        return false;
+      },
+      child: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+              url: Uri.parse(
+                  'https://ssoda.notion.site/492cd8758ae44accb1986d25ee656ee5')),
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          initialOptions: options,
+        ),
+      ),
+    ));
   }
 }
