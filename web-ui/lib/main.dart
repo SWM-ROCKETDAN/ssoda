6ed6dcd454f8db2afb70eeccff0db062a9c071d0
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-10-12 15:45:28 +0900

    support naver blog platform

--- web-ui/lib/main.dart
+++ web-ui/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
           scaffoldBackgroundColor: kScaffoldBackgroundColor,
           accentColor: kShadowColor),
-      home: HomeScreen(),
+      home: StoreEventScreen(storeId: 1),
     );
   }
 
