b918d0a74e969ac38ce9ef21a53a6409a7d159bd
iamhge <fine632@gmail.com>
2021-10-15 17:01:45 +0900

    add Event Report entity

--- service-server/src/main/java/com/rocketdan/serviceserver/domain/event/EventRepository.java
+++ service-server/src/main/java/com/rocketdan/serviceserver/domain/event/EventRepository.java
@@ -3,8 +3,6 @@ package com.rocketdan.serviceserver.domain.event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
