163931a45e243423f2c295536ed4789257ce1cb9
송영모 <mooyoung2309@gmail.com>
2021-10-28 17:14:35 +0900

    pytest: report test code

---
+++ analysis-server/server/report/test/test_report.py
@@ -0,0 +1,29 @@
+from rest_framework.reverse import reverse
+from core.models import JoinPost
+from core.models import JoinUser
+from core.exceptions import exceptions
+from config.settings.base import get_secret
+from datetime import datetime
+import pytest
+
+TEST_NAVER_BLOG_URL = get_secret("TEST_NAVER_BLOG_URL")
+
+
+@pytest.mark.urls(urls="config.urls")
+@pytest.mark.django_db(transaction=True)
+class TestReportEvent:
+    def test_report_events_event_report_calculate_ok(self, client, event1_for_report_api):
+        url = reverse(viewname="report_events", args=[event1_for_report_api.id, ])
+        response = client.get(url)
+
+        assert response.json()["message"] == exceptions.EventReportCalculateOK.default_detail
+
+
+@pytest.mark.urls(urls="config.urls")
+@pytest.mark.django_db(transaction=True)
+class TestReportStore:
+    def test_report_stores_store_report_calculate_ok(self, client, store1_for_report_api):
+        url = reverse(viewname="report_stores", args=[store1_for_report_api.id, ])
+        response = client.get(url)
+
+        assert response.json()["message"] == exceptions.StoreReportCalculateOK.default_detail
