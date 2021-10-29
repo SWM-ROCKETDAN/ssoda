501ee6e1c179becacd5cc87df02642f32f7bb671
DESKTOP-EA4HSD4\yjyoon <yjyoon.dev@gmail.com>
2021-10-29 23:44:58 +0900

    add reward name on push noti message

--- web-ui/lib/models/fcm.dart
+++ web-ui/lib/models/fcm.dart
@@ -11,9 +11,9 @@ class FCMessage {
       required this.data});
 }
 
-FCMessage createEventJoinNotification(String eventName) {
+FCMessage createEventJoinNotification(String eventName, String rewardName) {
   final title = '이벤트 참여 알림';
-  final body = '고객님이 $eventName에 참여했습니다!';
+  final body = '고객님이 $eventName에서 $rewardName에 당첨되셨습니다!';
   final image = "";
   final Map<String, String> data = {};
   return FCMessage(title: title, body: body, image: image, data: data);
